540b3abd0747101c69a509a4c2c455611bd6a73b
Jeong Jinwoo <jwjeong96@gmail.com>
2024-02-08 01:49:08 +0900

    feat: add Swagger dependency and setup Docket

---
+++ src/main/java/com/wafflestudio/babble/common/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.wafflestudio.babble.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+            .apiInfo(getApiInfo())
+            .select()
+            .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+            .paths(PathSelectors.any())
+            .build();
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfoBuilder().title("Babble API Documentation")
+            .version("beta")
+            .description("Location-based Anonymous Chat App")
+            .build();
+    }
+}
