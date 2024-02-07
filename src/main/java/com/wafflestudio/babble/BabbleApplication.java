ba333bf903017b27561dc942afdbd05e032dc370
Jeong Jinwoo <jwjeong96@gmail.com>
2024-02-08 00:29:29 +0900

    Initial Commit

---
+++ src/main/java/com/wafflestudio/babble/BabbleApplication.java
@@ -0,0 +1,12 @@
+package com.wafflestudio.babble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabbleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BabbleApplication.class, args);
+    }
+}
---
+++ src/test/java/com/wafflestudio/babble/BabbleApplicationTests.java
@@ -0,0 +1,12 @@
+package com.wafflestudio.babble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabbleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
