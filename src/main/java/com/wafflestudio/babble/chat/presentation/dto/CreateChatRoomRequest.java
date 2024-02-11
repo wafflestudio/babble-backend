2e13dd2b7d22773607f3d75394ac81289ad985c1
Jeong Jinwoo <jwjeong96@gmail.com>
2024-02-11 21:04:26 +0900

    doc: add @ApiModelProperty for createRoom API

--- src/main/java/com/wafflestudio/babble/chat/presentation/dto/CreateChatRoomRequest.java
+++ src/main/java/com/wafflestudio/babble/chat/presentation/dto/CreateChatRoomRequest.java
@@ -2,6 +2,7 @@ package com.wafflestudio.babble.chat.presentation.dto;
 
 import com.wafflestudio.babble.chat.application.dto.CreateChatRoomDto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,8 +11,11 @@ import lombok.NoArgsConstructor;
 @Getter
 public class CreateChatRoomRequest extends LocationRequest {
 
+    @ApiModelProperty(required = true)
     private String nickname;
+    @ApiModelProperty(required = true)
     private String roomName;
+    @ApiModelProperty(required = true, allowableValues = "LECTURE_ROOM, CAFETERIA, RESTAURANT, LIBRARY, DEPARTMENT_ROOM, CLUB_ACTIVITY_ROOM, CAFE")
     private String hashTag;
 
     public CreateChatRoomDto toDto(String authUserId) {
--- src/main/java/com/wafflestudio/babble/chat/presentation/dto/LocationRequest.java
+++ src/main/java/com/wafflestudio/babble/chat/presentation/dto/LocationRequest.java
@@ -1,5 +1,6 @@
 package com.wafflestudio.babble.chat.presentation.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -10,6 +11,8 @@ import lombok.NoArgsConstructor;
 @Getter
 public class LocationRequest {
 
+    @ApiModelProperty(required = true, value = "위도 - 얼마나 북쪽인가 (-90.0 ~ 90.0)")
     protected Double latitude;
+    @ApiModelProperty(required = true, value = "경도 : 얼마나 동쪽인가 (-180.0 ~ 180.0)")
     protected Double longitude;
 }
