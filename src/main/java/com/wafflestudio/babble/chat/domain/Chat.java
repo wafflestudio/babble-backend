4c7774c73bb0089eb8d7b40d320e90a4e6896858
Jeong Jinwoo <jwjeong96@gmail.com>
2024-02-11 22:26:34 +0900

    feat: setup Chat domain and ChatRepository

---
+++ src/main/java/com/wafflestudio/babble/chat/domain/Chat.java
@@ -0,0 +1,37 @@
+package com.wafflestudio.babble.chat.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Chat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_room_id", nullable = false)
+    private ChatRoom room;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chatter_id", nullable = false)
+    private Chatter chatter;
+
+    @Column(nullable = false)
+    private String content;
+}
