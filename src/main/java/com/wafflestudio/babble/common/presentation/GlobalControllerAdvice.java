7786e68ef549306822c06ab779650a1f785a9fd6
Jeong Jinwoo <jwjeong96@gmail.com>
2024-02-11 21:14:38 +0900

    fix: update default error message formats
    - english to korean
    - remove unnecessary format

--- src/main/java/com/wafflestudio/babble/common/presentation/GlobalControllerAdvice.java
+++ src/main/java/com/wafflestudio/babble/common/presentation/GlobalControllerAdvice.java
@@ -20,13 +20,13 @@ import lombok.extern.slf4j.Slf4j;
 @ControllerAdvice
 public class GlobalControllerAdvice {
 
-    private static final String INTERNAL_SERVER_ERROR_MESSAGE = "Internal Server Error";
-    private static final String INVALID_REQUEST_EXCEPTION_MESSAGE_FORMAT = "Invalid Request: [%s]";
-    private static final String INVALID_REQUEST_DELIMITER = ", ";
+    private static final String INTERNAL_SERVER_ERROR_MESSAGE = "알 수 없는 문제가 발생했습니다.";
+    private static final String INVALID_REQUEST_EXCEPTION_MESSAGE_FORMAT = "잘못된 요청입니다. %s";
+    private static final String INVALID_REQUEST_DELIMITER = " ";
 
     @ExceptionHandler
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
-        log.error("UnhandledException has been thrown : ", e);
+        log.error("UnhandledException has been thrown", e);
         return new ResponseEntity<>(new ErrorResponse(INTERNAL_SERVER_ERROR_MESSAGE), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
